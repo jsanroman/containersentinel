5f32158addd9f07e1b916473aa067f5b21ea398a
Alex Hermida <alexhermida@gmail.com>
2016-02-27 15:24:14 +0100

    add upload view

--- load_control/core/views.py
+++ load_control/core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.parsers import FileUploadParser
 
-# Create your views here.
+
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+class FileUploadView(APIView):
+    parser_classes = (FileUploadParser,)
+
+    def put(self, request, filename, format=None):
+        file_obj = request.data['file']
+
+        return Response(status=204)
--- load_control/load_control/urls.py
+++ load_control/load_control/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from core import views
+
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^upload/$', views.FileUploadParser),
+    url(r'^$', views.home),
+
 ]
