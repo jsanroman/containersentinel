3c48d0ea90c54116e5a3bd4d90d1d123de888bc5
Javi <sanroman.javier@gmail.com>
2016-02-28 11:10:22 +0100

    factory to measurements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+//= require lodash
 //= require jquery
 //= require jquery_ujs
 //= require foundation
@@ -10,29 +11,40 @@
 
 var load_control = angular.module('load_control', ["highcharts-ng"]).run(['$rootScope', function($rootScope) {
   $rootScope.urlBase = urlBase;
-}]);
+}])
+.constant('_', window._);
 
 
-load_control.controller('dashboard_ctrl', ['$scope', function ($scope) {
-    $scope.tempGraphic = {
-        series: [
-          {
-            name: 'Area',
-            type: 'areaspline',
-            data: [[1456602522555,-12],[1456602517564,6],[1456602516056,10],[1456602511535,3]],
-            pointStart: Date.UTC(2011, 0),
-            pointInterval: 30 * 24 * 36e5,
-            color: '#FF0000',
-            negativeColor: '#0088FF'
+load_control.controller('dashboard_ctrl', ['$scope', 'measureFactory', function ($scope, measureFactory) {
+
+
+    measureFactory.find().success(function(data){
+      data2 = _.map(data, function (line) {
+        return [line.timestamp, line.data1];
+      })
+
+      $scope.tempGraphic = {
+          series: [
+            {
+              name: 'Area',
+              type: 'areaspline',
+              data: data2,
+              pointStart: Date.UTC(2011, 0),
+              pointInterval: 30 * 24 * 36e5,
+              color: '#FF0000',
+              negativeColor: '#0088FF'
+            }
+          ],
+          func: function (chart) {
+              console.log(chart);
+              $scope.chartData = chart;
+              $scope.chartExport = $.proxy(chart.exportChart, chart);
           }
-        ],
+      }
 
-        func: function (chart) {
-            console.log(chart);
-            $scope.chartData = chart;
-            $scope.chartExport = $.proxy(chart.exportChart, chart);
-        }
-    }
+    }).error(function(response, status) {
+    });
+
 
 
     $scope.gyroGraphic = {
--- app/assets/javascripts/factories.js
+++ app/assets/javascripts/factories.js
@@ -1,9 +1,9 @@
 angular.module('load_control')
-  .factory('measureFactory', ['$http', function ($http) {
+  .factory('measureFactory', ['$http', '$rootScope', function ($http, $rootScope) {
     var factory = {};
 
     factory.find = function (key) {
-      return $http.get($rootScope.urlBase + '/api/v1/measures.json');
+      return $http.get($rootScope.urlBase + '/api/v1/measurements.json');
     }
 
     return factory;
