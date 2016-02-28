6e60eb0c9321fe49ecb31744819c1d56574b1be5
Javi <sanroman.javier@gmail.com>
2016-02-28 09:43:35 +0100

    parser data to create measurement

--- app/controllers/api/v1/measurements_controller.rb
+++ app/controllers/api/v1/measurements_controller.rb
@@ -1,10 +1,28 @@
+require 'date'
+
 class Api::V1::MeasurementsController < ApplicationController
   def index
     @measurements = Measurement.all
   end
 
   def create
-    Measurement.create(params.permit(:kind, :data1, :data2, :data3, :data4, :datetime, :device_id))
+    data  = params[:data].gsub(" ", "")
+    data  = URI.decode(data)
+    data = data.split("\r\n")
+
+    if data.size>0
+      data.each do |line|
+        line = line.split(",")
+
+        Measurement.create({device_id: 1, 
+                            kind: line[1], 
+                            data1: line[2], 
+                            data2: line[3], 
+                            data3: line[4], 
+                            data4: line[5], 
+                            datetime: Time.at(line[0].to_i/1000).to_datetime})
+      end
+    end
   end
 
 
